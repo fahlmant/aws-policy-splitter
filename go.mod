68bfd8e7d29e34182da03e310f65fbddd29b23c8
Taylor Fahlman <tfahlman@redhat.com>
2023-04-08 08:35:08 -0400

    Inital logic for splitting JSON policies

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.20
 require github.com/n4ch04/aws-policy v1.0.1
 
 require (
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
-	github.com/rs/zerolog v1.21.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	//"github.com/aws/aws-sdk-go/service/iam"
 	awsPolicy "github.com/n4ch04/aws-policy"
+	awspolicy "github.com/n4ch04/aws-policy"
 )
 
 const (
-	policyDocVersion = "2012-10-17"
+	// https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_iam-quotas.html#reference_iam-quotas-entity-length
+	// "The size of each managed policy cannot exceed 6,144 characters."
+	managedPolicyCharLimit = 6144
+	policyVersionString    = "2012-10-17"
+	// The number of characters for the version string and {} in the json
+	characterCountBuffer = 26
 )
 
 func main() {
@@ -28,26 +36,93 @@ func main() {
 		fmt.Println(err)
 	}
 
-	err = buildPolicy(string(filedata))
+	if len(filedata) < managedPolicyCharLimit {
+		fmt.Printf("No splitting needed, policy size of %d does not exceed max size of %d\n", len(filedata), managedPolicyCharLimit)
+		return
+	}
+
+	err = splitPolicy(filename, filedata)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func buildPolicy(policyString string) error {
+func splitPolicy(filename string, filecontents []byte) error {
 
-	policy := awsPolicy.Policy{}
+	fileAsPolicy := awsPolicy.Policy{}
 
-	err := policy.UnmarshalJSON([]byte(policyString))
+	err := fileAsPolicy.UnmarshalJSON(filecontents)
 	if err != nil {
 		return err
 	}
 
-	policyJSON, err := json.MarshalIndent(policy, "", "  ")
-	if err != nil {
-		fmt.Println(err)
+	var newPoliciesToBuild []awsPolicy.Policy
+	statementIndex := 0
+	// Loop until we're at the end of statements
+	for {
+		newPolicy := awspolicy.Policy{}
+		totalSize := characterCountBuffer
+		for {
+			statement := fileAsPolicy.Statements[statementIndex]
+			// Convert the struct to JSON to count characters
+			statementAsJSON, err := getJSONfromStatemet(statement)
+			if err != nil {
+				return err
+			}
+
+			if totalSize+len(statementAsJSON) < managedPolicyCharLimit {
+				newPolicy.Statements = append(newPolicy.Statements, statement)
+				statementIndex += 1
+			} else {
+				break
+			}
+		}
+		newPoliciesToBuild = append(newPoliciesToBuild, newPolicy)
+		if statementIndex == len(fileAsPolicy.Statements) {
+			break
+		}
 	}
-	fmt.Print(string(policyJSON))
+
+	for i, policy := range newPoliciesToBuild {
+		// Build file name for new file
+		newFilename := fmt.Sprintf("%s-%d.json", fileNameWithoutExtTrimSuffix(filename), i)
+		// Add version to policy
+		policy.Version = policyVersionString
+		//Create file
+		contents, err := getJSONfromPolicy(policy)
+		if err != nil {
+			return err
+		}
+
+		err = os.WriteFile(newFilename, contents, 0644)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
+}
+
+func getJSONfromStatemet(statement awsPolicy.Statement) ([]byte, error) {
+	jsonStatemet, err := json.Marshal(statement)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return jsonStatemet, nil
+}
+
+func getJSONfromPolicy(policy awsPolicy.Policy) ([]byte, error) {
+	jsonPolicy, err := json.Marshal(policy)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return jsonPolicy, nil
+}
+
+func fileNameWithoutExtTrimSuffix(fileName string) string {
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
 }
